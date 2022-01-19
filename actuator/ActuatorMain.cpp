845fe36700017628822d3bc64f7507b485a64eb1
Matthew Pham <mattpham@udel.edu>
2022-01-19 12:15:29 -0500

    mp

--- actuator/ActuatorMain.cpp
+++ actuator/ActuatorMain.cpp
@@ -24,13 +24,37 @@ using namespace std;
 int main()
 {
     int deviceID = 0;
-    vector<string> serialNumba;
+    //vector<string> serialNumba;
     TLI_BuildDeviceList();
     // get device list size 
     short n = TLI_GetDeviceListSize();
-	cout << n << endl;
-	system("pause");
-	return 0;
+    cout << n << endl;
+    system("pause");
+
+
+    const char serialNumba = 27260232;
+    CC_Open(&serialNumba);
+    CC_Home(&serialNumba);
+    //if (CC_CanMoveWithoutHomingFirst(&serialNumba)) {
+    //    cout << "yay";
+    //system("pause");
+    cout << CC_GetHomingVelocity(&serialNumba) << endl;
+    // CC_RequestHomingParams(&serialNumba);
+
+     //CC_GetHomingParamsBlock(&serialNumba)
+
+    cout << CC_CanHome(&serialNumba) << endl;
+    system("pause");
+
+    int device_unit = 0;
+    int unitType = 0;
+    double real_unit = 1;
+
+    CC_GetDeviceUnitFromRealValue(&serialNumba, real_unit, &device_unit, unitType);
+    cout << device_unit << endl;
+    system("pause");
+    CC_MoveToPosition(&serialNumba, device_unit);
+    return 0;
 
 }
 
