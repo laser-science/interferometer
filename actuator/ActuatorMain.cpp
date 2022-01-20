63b7141f88d454971f4d5cf0aaf5d97235625e51
Matthew Pham <mattpham@udel.edu>
2022-01-20 14:55:00 -0500

    imagine not being able to commit lol

--- actuator/ActuatorMain.cpp
+++ actuator/ActuatorMain.cpp
@@ -16,45 +16,61 @@
 #include <cstdlib>
 #include <time.h>
 #include <stdio.h>
+#include <conio.h>
 
 #include "Thorlabs.MotionControl.KCube.DCServo.h"
-
+    
 using namespace std;
 
 int main()
 {
     int deviceID = 0;
-    //vector<string> serialNumba;
-    TLI_BuildDeviceList();
+    //vector<string> serialNumba = 27260232;
+    int err = TLI_BuildDeviceList();
+    cout << err << endl;
+    system("pause");
     // get device list size 
     short n = TLI_GetDeviceListSize();
     cout << n << endl;
     system("pause");
 
+    // trying to open communication with the device
+    char serialNumba = '1';
+    if (CC_Open(&serialNumba) == 0)
+    {
+        cout << "Device is open" << endl;
+    }
+    // Checking the connection to the device 
+    if (CC_CheckConnection(&serialNumba) == true)
+    {
+        cout << "Device is connected" << endl;
+    }
+    // Check if we can perform a home
+    if (CC_CanHome(&serialNumba) == true)
+    {
+        cout << "The device can perform a home" << endl;
+    }
 
-    const char serialNumba = 27260232;
-    CC_Open(&serialNumba);
-    CC_Home(&serialNumba);
-    //if (CC_CanMoveWithoutHomingFirst(&serialNumba)) {
-    //    cout << "yay";
-    //system("pause");
-    cout << CC_GetHomingVelocity(&serialNumba) << endl;
-    // CC_RequestHomingParams(&serialNumba);
+    // setting the relative distance movement
+    int distance = 10000;
+    err = CC_SetMoveRelativeDistance(&serialNumba, distance);
+    cout << err << endl;
+    CC_MoveRelativeDistance(&serialNumba);
+    //cout << CC_GetHomingVelocity(&serialNumba) << endl;
+   // CC_GetHomingParamsBlock(&serialNumba, MOT_HomingParameters *homingParams);
 
-     //CC_GetHomingParamsBlock(&serialNumba)
-
-    cout << CC_CanHome(&serialNumba) << endl;
+    cout << CC_Home(&serialNumba) << endl;
     system("pause");
 
     int device_unit = 0;
     int unitType = 0;
-    double real_unit = 1;
+    double real_unit = 6;
 
     CC_GetDeviceUnitFromRealValue(&serialNumba, real_unit, &device_unit, unitType);
     cout << device_unit << endl;
     system("pause");
-    CC_MoveToPosition(&serialNumba, device_unit);
+    // CC_MoveToPosition(&serialNumba, device_unit);
+
     return 0;
-
+    
 }
-
