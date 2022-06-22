5f999862622373ab4ce489ff3923689b9cc606eb
Andreuz1 <97983556+Andreuz1@users.noreply.github.com>
2022-06-22 16:50:09 -0400

    Fixed ZA

--- interface/TestingAll.cpp
+++ interface/TestingAll.cpp
@@ -4144,7 +4144,7 @@ int writeToFRG(int numScans, double lambdaCenter, double temporalCalibration, do
 	ofstream MyFile;
 	MyFile.open("frogfile.frg", ios::app);
 	MyFile << width << " " << height << " " << temporalCalibration << " " << spatialCalibration << " " << lambdaCenter << endl;
-	for (int i = 1; i < numScans + 1; i++) {
+	for (int i = 0; i < numScans; i++) {
 		for (int j = 0; j < numScans; j++) {
 			MyFile << interArray[j][i] << " ";
 		}
